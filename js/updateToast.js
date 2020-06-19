3102fa8de78cdcaaea75b028b4db2516e1d7e17e
Ronaldo J Oliveira <ronaldojunio@gmail.com>
2020-06-19 16:35:00 -0300

    Limpeza no código

--- js/updateToast.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $("#toast-grafico").toast({
-      autohide: true,
-      delay: 5000
-    });
-    setTimeout(function () {
-      $("#toast-grafico").toast("show");
-    }, "1500");
-});
